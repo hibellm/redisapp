d2297e81ff40a202f4cf8db058ec1118d352669b
Marcus <marcus.hibell@roche.com>
2017-12-04 22:57:35 +0100

    Updates

--- redisapp.py
+++ redisapp.py
@@ -18,41 +18,57 @@ app.config.update(
 r = redis.StrictRedis(host='192.168.99.100', port=32771, db=0, charset="utf-8", decode_responses=True)
 pipe = r.pipeline()
 
-pipe.set('foo', 'bar').sadd('faz', 'baz').incr('auto_number').execute()
-pipe.sadd('drug001', 'pt101','pt102','pt108','pt999').execute()
-pipe.sadd('drug002', 'pt103','pt104','pt105','pt106','pt107','pt109','pt123','pt456','pt120').execute()
-pipe.sadd('ae001', 'pt120','pt101','pt107','pt109','pt123','pt456').execute()
-pipe.sadd('ae002', 'pt101','pt103','pt104').execute()
+# pipe.set('foo', 'bar').sadd('faz', 'baz').incr('auto_number').execute()
+# pipe.sadd('drug001', 'pt101','pt102','pt108','pt999').execute()
+# pipe.sadd('drug002', 'pt103','pt104','pt105','pt106','pt107','pt109','pt123','pt456','pt120').execute()
+# pipe.sadd('ae001', 'pt120','pt101','pt107','pt109','pt123','pt456').execute()
+# pipe.sadd('ae002', 'pt101','pt103','pt104').execute()
+# pipe.hmset('drugunit', 'drug001 ','pt103','pt104').execute()
+
 pipe.sinterstore('d1a1','drug001','ae001').sinterstore('d1a2','drug001','ae002').sinterstore('d2a1','drug002','ae001').sinterstore('d2a2','drug002','ae002').execute()
 
 def percent(num1, num2):
-    return '{0:.2%}'.format((num1 / num2))
-
+    return ' ({0:.2%}'.format((num1 / num2))+")"
 
 # Index
 @app.route('/')
 def index():
 
     x=(r.smembers('drug001'))
-    print('drug001 has ',x)
+    # print('drug001 has ',x)
     x=(r.smembers('ae001'))
-    print('ae001 has ',x)
+    # print('ae001 has ',x)
     x=(r.smembers('test'))
-    print('test has ',x)
-    x=(r.hmget('aelist','ae001'))
-    y=(r.hmget('aelist','ae002'))
+    # print('test has ',x)
+    x=''.join(r.hmget('aelist','ae001')) #This was a list converted to a string
+    y=''.join(r.hmget('aelist','ae002'))
+    drg1=''.join(r.hmget('druglist','drug001')+r.hmget('drugdose','drug001')+r.hmget('drugunit','drug001'))
+    drg2=''.join(r.hmget('druglist','drug002')+r.hmget('drugdose','drug002')+r.hmget('drugunit','drug002'))
+    drg999=''.join(r.hmget('druglist','drug999')+r.hmget('drugdose','drug999')+r.hmget('drugunit','drug999'))
+    print(drg1)
     drug001=(r.scard('drug001'))
     drug002=(r.scard('drug002'))
+    drug999=(drug001+drug002)
 
-    d1a1=r.scard('d1a1')," ({:.2%}".format((r.scard('d1a1')/r.scard('drug001')))
-    d1a2=r.scard('d1a2')," ({:.2%}".format((r.scard('d1a2')/r.scard('drug001')))
-    d2a1=r.scard('d2a1')," ({:.2%}".format((r.scard('d2a1')/r.scard('drug002')))
-    d2a2=r.scard('d2a2')," ({:.2%}".format((r.scard('d2a2')/r.scard('drug002')))
-    z=r.scard('d2a2'),percent(r.scard('d2a2'),r.scard('drug002'))
-    print('test has ',z ,' elements')
-    return render_template('home.html',x=x,y=y,z=z,drug001=drug001,drug002=drug002, d1a1=d1a1,d1a2=d1a2,d2a1=d2a1,d2a2=d2a2)
+    d1a1=str(r.scard('d1a1'))+str(percent(r.scard('d1a1'),r.scard('drug001')))
+    d1a2=str(r.scard('d1a2'))+str(percent(r.scard('d1a2'),r.scard('drug001')))
+    d2a1=str(r.scard('d2a1'))+str(percent(r.scard('d2a1'),r.scard('drug002')))
+    d2a2=str(r.scard('d2a2'))+str(percent(r.scard('d2a2'),r.scard('drug002')))
 
-# {'attrs': ['a', 'b', 'c']}
+    # d9a1=str(d1a1+d2a1)+str(percent(d1a1+d2a1,drug999))
+    z=str(r.scard('d2a2'))+str(percent(r.scard('d2a2'),r.scard('drug002')))
+
+    # reqdrug=['drug001','drug002']
+    # reqae=['ae001','ae002','ae003']
+    # for drug in reqdrug:
+    #     for ae in reqae:
+    #         d1a1=str(r.scard('d1a1'))+str(percent(r.scard('d1a1'),r.scard('drug001')))
+    #         print('the first drug is ' ,drug)
+
+
+    # print('test has ',z ,' elements')
+    # return render_template('home.html',x=x,y=y,z=z,drug001=drug001,drug002=drug002, d1a1=d1a1,d1a2=d1a2,d2a1=d2a1,d2a2=d2a2)
+    return render_template('home.html', **locals() )
 
 # WORKS!
 # import redis
